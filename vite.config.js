af664695fdc8c21ec1f2825e1efc0659cd2c0d84
sexysanghak <totoriverce@gmail.com>
2026-03-24 21:23:56 +0900

    fix: vite base 경로 수정 및 분기

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// 배포 대상 판단
+const isGithub = process.env.VITE_DEPLOY_TARGET === "github";
+
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: isGithub ? "/risk-mgmt-system/" : "/",
 });
